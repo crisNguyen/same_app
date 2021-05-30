29f061f7176e444a21a1a73f3baa5a8a81973c5a
Trieu Nguyen <trieunv@imt-soft.com>
2021-05-30 18:27:00 +0700

    Fisnish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   get  'static_pages/home'
   get  'static_pages/contact'
-
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

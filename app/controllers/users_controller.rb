07ae4c87a7faa24fb141d87f3451c835b44ed11f
Trieu Nguyen <trieunv@imt-soft.com>
2021-06-02 22:49:23 +0700

    + Implement login_logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in(@user)
+      remember(@user)
       flash[:success] = "Welcome to the Sam App!"
       redirect_to @user
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+
   get 'users/new'
-  root'static_pages#home'
+  root 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/about'
-  get  'static_pages/home'
-  get  'static_pages/contact'
+  get 'static_pages/home'
+  get 'static_pages/contact'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+
+  delete 'logout', to:  'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

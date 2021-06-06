1a0d032dd06ef28eaa32e9141f845491e66f2803
Trieu Nguyen <trieunv@imt-soft.com>
2021-06-06 16:29:38 +0700

    + Implement account activations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,10 +14,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      byebug
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_path
       log_in(@user)
       remember(@user)
       flash[:success] = "Welcome to the Sam App!"
-      redirect_to @user
+      return @user
     else
       render 'new'
     end
